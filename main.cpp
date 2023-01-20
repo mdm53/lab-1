05543c61bc8afacb1c1851760055690b425bc098
mdm53 <123202490+mdm53@users.noreply.github.com>
2023-01-20 16:21:12 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main () {
+  cout<<"-------------------------------"<< endl;
+  cout << "Michael Malone" << endl;
+  cout<< "Computer Science 1"<<endl;
+  cout<< "Lab1"<< endl;
+  cout<< "January 20, 2023" << endl;
+  cout<<"-------------------------------"<< endl;
+  cout<<"lab1"<<endl;
+  int x; // integer variable
+  int y; // integer variable;
+  x = 20;
+  //cout<<"content of X right after initialization: " << x + y<<endl;
+  y = 5;
+  cout<<"X + Y = " << x + y<<endl;
+  //cout<<"content of y right before program exit: " << y << endl;
+
+  return 0;
+}
